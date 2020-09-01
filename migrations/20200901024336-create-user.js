4e84a6ffdde248de2f8ccb4970c8fcc0124111c2
handikaadicky <36119932+handikaadicky@users.noreply.github.com>
2020-09-01 13:34:22 +0700

    add migration

---
+++ migrations/20200901024336-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      date_birth: {
+        type: Sequelize.DATEONLY
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATEONLY
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATEONLY
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
