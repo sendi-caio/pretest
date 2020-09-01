b030376ed15621e20f5712f18f57b31c2f5980c4
muhammad sandi <muhammadsandimaulana12@gmail.com>
2020-09-01 10:59:52 +0700

    initial-users

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('empty test case')
+const express = require ('express')
+const app = express()
+const portfinder = require ('portfinder')
+const port = 4000
+
+app.get('/',  (req,res)=> res.send('server'))
+app.get ('/login',(req,res) =>res.send('login'))
+app.get('/dashboard',(req,res) =>res.send('dashboard'))
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+
+
