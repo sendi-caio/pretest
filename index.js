5088b418a10e562e8207a3ab4baebb362090a23e
ricky3knowhere <ricky03senju@gmail.com>
2020-09-01 10:18:55 +0700

    create login & dashboard routes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('empty test case')
+const express = require('express')
+const app = express()
+
+const config = require('./config')
+
+const login = require('./routes/login')
+
+const hbs = require('express-hbs');
+ 
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views',
+  defaultLayout: __dirname + '/views/default.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.get('/login', login.get)
+app.post('/login', login.post)
+
+app.get('/dashboard', (req, res) => {
+  res.send('dashboard')
+})
+
+app.get('/', (req, res) => {
+  res.redirect('login')
+})
+
+app.listen(config.port, () => {
+  console.log('Server running at port '+ config.port);
+})
---
+++ routes/login.js
@@ -0,0 +1,12 @@
+const get = (req, res) => {
+  res.send('login')
+}
+
+const post = (req, res) => {
+  res.send('login')
+}
+
+module.exports = {
+  get,
+  post
+}
