8ba845e40ebddec2f76261e383a74c499e033aa0
Akmaluddin <akmaluddinfadhilah@gmail.com>
2020-09-02 11:10:54 +0700

    create user & file upload
    - add feature create users, add form create user
    
    - add function user details in user.js

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const app = express()
 const port = 4000
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
+const path = require('path')
+const { rootPath } = require('./helpers/path')
+
+const fileUpload = require('express-fileupload')
+app.use(fileUpload({
+    createParentPath : true,
+    debug : true
+}));
+
+app.use('/uploads', express.static('uploads'))
 
 const hbs = require('express-hbs');
-const path = require('path')
-const rootPath = path.resolve(__dirname)
 const viewsPath = path.join(rootPath, 'views')
 
 app.engine('hbs', hbs.express4({
@@ -37,12 +45,21 @@ const checkIsLogin = require('./routes/checkIsLogin')
 const dashboard = (req, res) => res.render('pages/dashboard')
 const login = require('./routes/login');
 const logout = require('./routes/logout')
+const users = require('./routes/users')
 
 app.get('/', (req, res) => res.redirect('/login'))
 app.get('/login', login.get)
 app.post('/login', login.post)
 app.get('/dashboard', checkIsLogin, dashboard)
 app.get('/logout', logout)
+
+//route user
+app.get('/users', users.list)
+app.get('/users/create', users.create_get)
+app.post('/users/create', users.create_post)
+app.get('/users/:id', users.detail)
+
+
 
 app.get('/check-db', (req, res) => {
     const db = require('./models')
